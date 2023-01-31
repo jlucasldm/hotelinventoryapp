95a083d51d35f3be4ed1a737b1a3a9a32709e027
lucas <jlucas.ldm@gmail.com>
2023-01-31 13:00:09 -0300

    built-in pipes finished

--- projects/hotelinventory/src/app/rooms/rooms.component.ts
+++ projects/hotelinventory/src/app/rooms/rooms.component.ts
@@ -28,7 +28,8 @@ export class RoomsComponent {
       price: 500,
       photos: 'placeholder',
       checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021')  
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.5  
     },
     {
       roomNumber: 2,
@@ -37,7 +38,8 @@ export class RoomsComponent {
       price: 1000,
       photos: 'placeholder',
       checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021')  
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 3.44897414561546  
     },
     {
       roomNumber: 3,
@@ -46,7 +48,8 @@ export class RoomsComponent {
       price: 1500,
       photos: 'placeholder',
       checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021')  
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 2.6   
     }
   ]
   
