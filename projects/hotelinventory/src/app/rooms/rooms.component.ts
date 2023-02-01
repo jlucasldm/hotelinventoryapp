bf100bd8215713d6a6d7da5a3e3c18c625ae209b
lucas <jlucas.ldm@gmail.com>
2023-02-01 12:29:19 -0300

    working on ngOnInit and component communication using input and output

--- projects/hotelinventory/src/app/rooms/rooms.component.ts
+++ projects/hotelinventory/src/app/rooms/rooms.component.ts
@@ -6,6 +6,7 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
+
 export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
@@ -20,39 +21,45 @@ export class RoomsComponent {
 
   hideRooms = false;
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 500,
-      photos: 'placeholder',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.5  
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000,
-      photos: 'placeholder',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 3.44897414561546  
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1500,
-      photos: 'placeholder',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 2.6   
-    }
-  ]
+  roomList: RoomList[] = []
   
+  constructor() {}
+
+  ngOnInit(): void{
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 500,
+        photos: 'placeholder',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5  
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1000,
+        photos: 'placeholder',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.44897414561546  
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1500,
+        photos: 'placeholder',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 2.6   
+      }
+    ]
+  }
+
   toggle(){
     this.hideRooms = !this.hideRooms;
   }
